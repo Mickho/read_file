00ac588ccde21ddd0baaa4dbe1ce01de133272e0
Mickho <mick.my.ho@gmail.com>
2019-01-19 14:21:08 +0800

    Add new file

---
+++ read_file.py
@@ -0,0 +1,6 @@
+# Read file
+data = [] #建立資料清單 
+with open('autotest.txt', 'r') as log: #with: 當程式迴圈結束時 檔案讀取會自動結束
+	for test in log:
+		data.append(test.strip())#將Test 裡面的資料append到data資料清單
+print(data) #並且將資料清單的資料印出來
