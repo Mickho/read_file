cd95eab1c36e387bb2bea3dab4cb0937a9cc4172
Mickho <46715814+Mickho@users.noreply.github.com>
2019-01-20 22:15:50 +0800

    Add files via upload
    add new print

--- read_test.py
+++ read_test.py
@@ -2,4 +2,8 @@ data = []
 with open('computer.txt', 'r') as log: 
 	for test in log:
 		data.append(test.strip())
-print(data)
+print(data) #印出清單資料
+print(len(data)) #印出有多少筆資料
+print(data[0]) #印出第一筆資料
+print('111111111111111')
+print(data[1]) #印出第二筆資料
